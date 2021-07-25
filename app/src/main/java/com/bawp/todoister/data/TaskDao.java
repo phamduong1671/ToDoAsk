0353415761bd2f136ed6872622ab87aa9b5b55cb
phamduong1671 <phamduong1671@users.noreply.github.com>
2021-07-25 11:16:46 +0700

    main

---
+++ app/src/main/java/com/bawp/todoister/data/TaskDao.java
@@ -0,0 +1,23 @@
+package com.bawp.todoister.data;
+
+import com.bawp.todoister.model.Task;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface TaskDao {
+
+    @Insert
+    void insertTask(Task task);
+
+    @Query("DELETE FROM task_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM task_table")
+    LiveData<List<Task>> getTask();
+}
